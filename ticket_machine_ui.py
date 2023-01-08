58d58a9ae003ae667add53308f72ce205a8d1bdf
Angelika Ostrowska <01178572@pw.edu.pl>
2023-01-08 15:29:16 +0100

    Creating a language system as dict in file

--- ticket_machine_ui.py
+++ ticket_machine_ui.py
@@ -1,5 +1,21 @@
-
+# Dictionary of message codes to ui messages in different languages
 messages = {
+    'choose_language': {
+        'ENG': 'Choose a language',
+        'PL': 'Wybierz jezyk'
+    },
+    'choose_action': {
+        'ENG': 'Choose an action',
+        'PL': 'Wybierz akcje'
+    },
+    'ENG': {
+        'ENG': 'English',
+        'PL': 'Angielski'
+    },
+    'PL': {
+        'ENG': 'English',
+        'PL': 'Angielski'
+    },
 
 }
 
@@ -13,11 +29,18 @@ def get_input(list_of_options, message):
     user_input = None
     while user_input not in range(len(list_of_options)):
         print_options(list_of_options)
-        user_input = int(input(message))
+        try:
+            user_input = int(input(message))
+        except ValueError:
+            user_input = None
     return list_of_options[user_input]
 
 
 def ui(messages):
     available_languages = ['ENG', 'PL']
-    language = get_input(available_languages, 'Choose a language')
-    pass
+    language = get_input(available_languages, 'choose_language')
+    main_menu_options = [
+        'buy_short_term_ticket',
+        'buy_long_term_ticket',
+        'other_long_term_ticket'
+        ]
