edd60346bed16381f1eecf985a7fa724b06ae201
Angelika Ostrowska <01178572@pw.edu.pl>
2023-01-11 03:41:37 +0100

    Added Long_Term_Prepaid_Ticket functionality

--- classes.py
+++ classes.py
@@ -23,6 +23,8 @@ class Ticket:
 class Long_Term_Time_Ticket(Ticket):
     def __init__(self, id, data):
         super().__init__(id, data)
+        # data should include: duration, date_of_purchase
+        # data could include: price, discount_type
 
     def check_status(self):
         current_date = date.today()
@@ -31,7 +33,7 @@ class Long_Term_Time_Ticket(Ticket):
         days_left = duration - (current_date - date_of_purchase)
         return max(days_left, timedelta(days=0))
 
-    def reload_ticket(self, added_duration):
+    def recharge_ticket(self, added_duration):
         days_left = self.check_status()
         new_date_of_purchase = date.today().isoformat()
         self.data['date_of_purchase'] = new_date_of_purchase
@@ -49,6 +51,18 @@ class Long_Term_Time_Ticket(Ticket):
 class Long_Term_Prepaid_Ticket(Ticket):
     def __init__(self, id, data):
         super().__init__(id, data)
+        # data should include: balance, active_ticket
 
-    # @TODO status checking for prepaid ticket
-    # @TODO reload ticket
+    def check_balance(self):
+        return float(self.data['balance'])
+
+    def recharge_ticket(self, value):
+        self.data['balance'] = self.check_balance() + value
+        self.save_to_file()
+        return 1
+
+    def use_prepaid(self, ticket, price):
+        self.data['current_ticket'] = ticket
+        self.data['balance'] = self.check_balance() - price
+        self.save_to_file()
+        return 1
